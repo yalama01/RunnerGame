45276fda05822e50ce8ab838181b0cb69ca04f0d
yalama01 <67355562+yalama01@users.noreply.github.com>
2022-01-04 23:23:40 -0500

    break

--- runner/Assets/Scripts/SpawnObsticals.cs
+++ runner/Assets/Scripts/SpawnObsticals.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 
 public class SpawnObsticals : MonoBehaviour
 {
+    // setting up vars
 
+
+    //calling obstical
+    public GameObject obstical;
+
+    //timing
+    float startupTime = 2f;
+    float spawnTime = 1.5f;
+    
+    //
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        //InvokeRepeating();
+        InvokeRepeating("spawobsticals",startupTime,spawnTime);
     }
 
     // spawns obsticals
-    void Update()
+    void spawobsticals()
     {
-        
+        Instantiate(obstical, transform.position , obstical.transform.rotation);
     }
 }
